2485cd2e1ef9ef95c4e99b5274259572483bbcdf
tapia81 <tapiaanthony81@gmail.com>
2022-06-05 22:11:20 -0400

    Added selling stock functionality. Added function if stock quantity is 0 delete stock and return to previous page. Added more css styling. Allowed transaction quantity to be adjusted by plus and minus buttons or type input. Added arithmetic for total transaction price and total asset of stock held.

--- client/src/components/routes/Stock.jsx
+++ client/src/components/routes/Stock.jsx
@@ -1,15 +1,16 @@
 import axios from "axios";
-import { useState, useEffect, Children } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 export const Stock = () => {
   const [selectedStock, setSelectedStock] = useState({});
   const [isRendered, setIsRendered] = useState(false);
-  const [isButtonClicked, setIsButtonClicked] = useState(false);
   const [updated, setUpdated] = useState(false);
+  const [deleted, setDeleted] = useState(false);
   const [currentStockQuantity, setCurrentStockQuantity] = useState(
     selectedStock.quantity
   );
+  const [transactionType, setTransactionType] = useState("buy");
   const [currentValue, setCurrentValue] = useState(1);
   const [updatedValue, setUpdatedValue] = useState(0);
   const [currentTotalPrice, setCurrentTotalPrice] = useState(0);
@@ -17,6 +18,15 @@ export const Stock = () => {
     quantity: 0,
   });
   const { id } = useParams();
+  let navigate = useNavigate();
+  const destroy = () => {
+    axios({
+      url: `${process.env.REACT_APP_API_URL}/api/stocks/${id}`,
+      method: "DELETE",
+    })
+      .then(() => setDeleted(true))
+      .catch(console.error);
+  };
 
   const fetchSingleStock = async () => {
     const response = await axios({
@@ -26,46 +36,56 @@ export const Stock = () => {
     setSelectedStock(response.data.stock);
     setCurrentStockQuantity(response.data.stock.quantity);
     setCurrentTotalPrice(selectedStock.stock_price);
+    if (selectedStock.quantity == 0) {
+      destroy();
+      return navigate("/dashboard");
+    }
   };
 
   useEffect(() => {
     fetchSingleStock();
   }, [id]);
 
-  const handleRenderingClick = () => {
-    if (!isRendered) {
+  const handleRenderingClick = (event) => {
+    if (event.target.name === "buy") {
       setIsRendered(true);
-    } else {
+      setTransactionType("buy");
+    } else if (event.target.name === "sell") {
+      setIsRendered(true);
+      setTransactionType("sell");
+    } else if (event.target.name === "cancel") {
       setIsRendered(false);
     }
   };
 
-  const handleButtonClicked = () => {
-    if (!isButtonClicked) {
-      setIsButtonClicked(true);
-    } else {
-      setIsButtonClicked(false);
-    }
-  };
-
-  const handleClick = (event) => {
+  const handleInputValueChange = (event) => {
     event.preventDefault();
     if (event.target.name == "plus") {
       setCurrentValue(currentValue + 1);
       setCurrentTotalPrice(currentTotalPrice + selectedStock.stock_price);
-    } else if (event.target.name == "minus") {
+    } else if (event.target.name == "minus" && currentValue >= 2) {
       setCurrentValue(currentValue - 1);
+      setCurrentTotalPrice(currentTotalPrice - selectedStock.stock_price);
+    } else if (event.target.name == "quantity" && currentValue >= 1) {
+      const typedInValue = Number(event.target.value);
+      setCurrentValue(typedInValue);
+      if (currentValue > typedInValue) {
+        setCurrentTotalPrice(currentTotalPrice / currentValue);
+      } else {
+        setCurrentTotalPrice(currentTotalPrice * typedInValue);
+      }
     }
   };
 
   const handleSubmit = (event) => {
-    console.log(`currentValue Input: ${currentValue}`);
-
     event.preventDefault();
-
-    if (!updated) {
+    console.log(event.target.name);
+    if (!updated && event.target.name === "buy") {
       setUpdated(true);
       setUpdatedValue(currentValue + currentStockQuantity);
+    } else {
+      setUpdated(true);
+      setUpdatedValue(currentStockQuantity - currentValue);
     }
     setCurrentValue(1);
   };
@@ -88,8 +108,6 @@ export const Stock = () => {
 
     if (updated) {
       handleUpdating();
-      console.log(`currentStockData:${currentStockQuantity}`);
-      console.log(`updatedValue:${updatedValue}`);
       setUpdated(false);
     }
     fetchSingleStock();
@@ -119,13 +137,14 @@ export const Stock = () => {
             <form
               onSubmit={(e) => handleSubmit(e)}
               className="quantity-container"
+              name={transactionType}
             >
               <input
                 type="button"
                 value="-"
                 className="minus"
                 name="minus"
-                onClick={(e) => handleClick(e)}
+                onClick={(e) => handleInputValueChange(e)}
               />
               <input
                 className="quantity"
@@ -135,6 +154,7 @@ export const Stock = () => {
                 max=""
                 name="quantity"
                 defaultValue={currentValue}
+                onChange={(e) => handleInputValueChange(e)}
                 pattern=""
                 inputMode=""
               />
@@ -143,7 +163,7 @@ export const Stock = () => {
                 defaultValue="+"
                 className="plus"
                 name="plus"
-                onClick={(e) => handleClick(e)}
+                onClick={(e) => handleInputValueChange(e)}
               />
             </form>
 
@@ -164,17 +184,27 @@ export const Stock = () => {
   return (
     <div className="selected-stock-container">
       <div className="purchasable-stock">
-        <p>{selectedStock.stock_name}</p>
-        <p>{selectedStock.stock_price}</p>
-        <p>{selectedStock.stock_symbol}</p>
-        <p>{selectedStock.quantity}</p>
-        <p>{selectedStock.totalCashValue}</p>
+        <p>Company: {selectedStock.stock_name}</p>
+        <p>Symbol: {selectedStock.stock_symbol}</p>
+        <p>Market Price: {selectedStock.stock_price}</p>
+        <p>Amount Held: {selectedStock.quantity}</p>
+        <p>
+          Asset Total: $
+          {(selectedStock.stock_price * selectedStock.quantity).toFixed(2)}
+        </p>
       </div>
 
       <div className="selected-stock-info">
         <div className="stock-buttons">
-          <button onClick={handleRenderingClick}>Buy</button>
-          <button onClick={handleRenderingClick}>Sell</button>
+          <button onClick={(e) => handleRenderingClick(e)} name="buy">
+            Buy
+          </button>
+          <button onClick={(e) => handleRenderingClick(e)} name="cancel">
+            Cancel
+          </button>
+          <button onClick={(e) => handleRenderingClick(e)} name="sell">
+            Sell
+          </button>
         </div>
 
         <HandleRendering />
