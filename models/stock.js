db273fb8cb9a2b919512fe6045a6fca9dac78ce2
tapia81 <tapiaanthony81@gmail.com>
2022-06-05 22:52:59 -0400

    added collection for user account assets

--- models/stock.js
+++ models/stock.js
@@ -1,16 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const Stock = new Schema(
-    {
-        stock_name: String,
-        stock_price: Number,
-        stock_symbol: String,
-        quantity: Number,
-        totalCashValue: Number
-    },
-    {timestamps: true}
-)
+  {
+    stock_name: String,
+    stock_price: Number,
+    stock_symbol: String,
+    quantity: Number,
+    totalCashValue: Number,
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("stocks", Stock)
+const Overview = new Schema({
+  account_value: Number,
+  buying_power: Number,
+});
+
+const StockModel = mongoose.model("stocks", Stock);
+const OverviewModel = mongoose.model("overview", Overview);
+
+module.exports = { StockModel, OverviewModel };
