9bb910deff1b0e138645f8a66be16719d2a67fdf
George <georgesucuzhanay100@gmail.com>
2022-06-07 23:09:12 -0400

    created dynamic dropdown menu for stockSymbols and render stockProfile

--- client/src/components/routes/Dashboard.jsx
+++ client/src/components/routes/Dashboard.jsx
@@ -2,16 +2,14 @@ import { Layout } from "../shared/Layout";
 import { DisplayStock } from "./DisplayStock";
 import { DisplayStocks } from "./DisplayStocks";
 import { TrendStocks } from "./TrendStocks";
-import { useState } from "react";
-import SingleStockView from "./SingleStockView";
 
 export const Dashboard = () => {
 
   'single-stock-select' // 'sss'
   'default-view' // 'dv'
  
-  const [mode, setMode] = useState('dv')
-  const [ticker, setTicker] = useState("");
+  // const [mode, setMode] = useState('dv')
+  // const [ticker, setTicker] = useState("");
 
   return (
     <Layout>
@@ -28,13 +26,19 @@ export const Dashboard = () => {
         {/* We are using lifting state up to track the stockTicker acrossFrom com1 into com2 */}
         {/* We are using another useState for the different screens of the right-side of the dashboard */}
 
-        {mode === 'dv' &&<div className="dashboard-right-elements">
+        {/* {mode === 'dv' &&<div className="dashboard-right-elements">
           <div className="api-stocks">
             <DisplayStock setMode={setMode} setTicker={setTicker}/>
           </div>
           <DisplayStocks />
         </div>}
-        {mode === 'sss' && <SingleStockView ticker={ticker} setMode={setMode} />}
+        {mode === 'sss' && <SingleStockView ticker={ticker} setMode={setMode} />} */}
+        <div className="dashboard-right-elements">
+          <div className="api-stocks">
+            <DisplayStock/>
+            </div>
+            <DisplayStocks />
+        </div>
 
 
 
