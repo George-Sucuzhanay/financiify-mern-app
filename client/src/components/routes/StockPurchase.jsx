192829c1c893e8d18a09385e0268319a221b0e0e
George <georgesucuzhanay100@gmail.com>
2022-06-20 22:06:28 -0400

    styled stockProfile and form for cleaner design

--- client/src/components/routes/StockPurchase.jsx
+++ client/src/components/routes/StockPurchase.jsx
@@ -42,41 +42,53 @@ export const StockPurchase = ({symbol, price}) => {
                 >
                 Return to Previous Page
               </button>  */}
-
+              <div className='subheadingTransaction2 '>
+                  <div className='transaction-column'>
+                      <h1>Enter Quantity</h1>
+                  </div>
+                  <div className='transaction-column'>
+                    <h1>Stock Price</h1>
+                  </div>
+                  <div className='transaction-column'>
+                    <h1>Stock Purchase</h1>
+                  </div>
+              </div>
                 <Form
                 handleSubmit={() => {}}
                 currentValue={quantity}
                 handleInputValueChange={(e) => setQuantity(e.target.value)}
                 selectedStock={{stock_price: price}}
                 currentTotalPrice={quantity * price}
-            />
-             <div className="">
-                <div className="stock-buttons">
-                <button
-                    onClick={(e) => handleRenderingClick(e)}
-                    name="buy"
-                    className="buy"
-                    style={{
-                    }}
-                >
-                    Buy
-                </button>
-                <button
-                    onClick={(e) => handleRenderingClick(e)}
-                    name="cancel"
-                    className="cancel"
-                >
-                    Cancel
-                </button>
-                <button
-                    onClick={(e) => handleRenderingClick(e)}
-                    name="sell"
-                    className="sell"
-                >
-                    Sell
-                </button>
+                />
+                <div className="transaction-row2 transaction-corners" >
+                    <div className="stock-buttons">
+                    <button
+                        onClick={(e) => handleRenderingClick(e)}
+                        name="buy"
+                        className="buy"
+                        style={{
+                        }}
+                    >
+                        Buy
+                    </button>
+                    <button
+                        onClick={(e) => handleRenderingClick(e)}
+                        name="cancel"
+                        className="cancel"
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        onClick={(e) => handleRenderingClick(e)}
+                        name="sell"
+                        className="sell"
+                    >
+                        Sell
+                    </button>
+                    </div>
                 </div>
-            </div>
+
+                
         </div>
     )
 }
